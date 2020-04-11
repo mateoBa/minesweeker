1fc95e47efd47b7053b7b098d27cf54e500a75bb
mateoba <mateobasaldua@gmail.com>
2020-04-11 17:13:17 -0300

    finishing game logic and refactoring migration by ended time in Game model

--- main/controllers.py
+++ main/controllers.py
@@ -1,5 +1,3 @@
-import datetime
-
 from random import randrange
 from main.models import *
 
@@ -7,7 +5,6 @@ from main.models import *
 class MatrixController:
     def __init__(self, columns, rows, mines):
         self.matrix = Matrix.objects.create(columns=columns, rows=rows, mines=mines)
-        self.matrix.save()
 
     def get_mines_in_matrix(self):
         mines = []
@@ -18,54 +15,57 @@ class MatrixController:
 
         return mines
 
-    def _analyze_and_sum(self, box_query):
-        if box_query:
-            box_query.first().sum()
-
-    def _calculate_adjacent_boxes(self, x, y):
+    def _adjacent_boxes(self, x, y, fun):
         for xn in range(x - 1, x + 2):
             for yn in range(y - 1, y + 2):
-                if xn == x and yn == y:
-                    continue
-                self._analyze_and_sum(Box.objects.filter(matrix=self.matrix, x=xn, y=yn))
+                fun(xn, yn)
+
+    def _sum_adjacent_boxes(self, x, y):
+        box_query = Box.objects.filter(matrix=self.matrix, x=x, y=y)
+        if box_query and box_query.first().value == Box.MINE:
+            box_query.first().sum()
 
     def draw_matrix(self):
         mines = self.get_mines_in_matrix()
         for x in range(self.matrix.columns):
             for y in range(self.matrix.rows):
                 box = Box.objects.create(x=x, y=y, matrix=self.matrix)
-
                 if (x, y) in mines:
-                    box.value = Box.MINE
-                    self._calculate_adjacent_boxes(x, y)
-
-                box.save()
+                    box.put_mine()
+                    self._adjacent_boxes(x, y, self._sum_adjacent_boxes)
 
     def get_matrix(self):
         self.draw_matrix()
         return self.matrix
 
-    def press(self, x, y):
-        pass
+    def _press(self, box):
+        box.press()
+        if box.value == Box.MINE:
+            return False
+
+        if box.value == 0:
+            self._adjacent_boxes(box.x, box.y, self.check_and_press)
+        return True
+
+    def check_and_press(self, x, y):
+        box = Box.objects.filter(matrix=self.matrix, x=x, y=y)
+        if box and box.first().is_hidden:
+            return self._press(box.first())
+
+    def get_boxes(self, is_hidden):
+        return Box.objects.filter(matrix=self.matrix, is_hidden=is_hidden)
 
 
 class GameController:
     def __init__(self, user, columns, rows, mines):
         self.matrix_controller = MatrixController(columns, rows, mines)
         self.game = Game.objects.create(user=user, matrix=self.matrix_controller.get_matrix())
-        self.game.save()
 
-    def start_game(self):
-        self.game.status = Game.STARTED
-        self.game.started_time = datetime.datetime.now()
-
-    def check_status(self):
-        if self.game.status == Game.INITIAL:
-            self.start_game()
-        else:
-            # change to win or lost
-            pass
-
-    def press_box(self, x, y):
-        self.matrix_controller.press(x, y)
-        self.check_status()
+    def press_box_and_check_status(self, x, y):
+        if not self.matrix_controller.get_boxes(is_hidden=False):
+            self.game.start_game()
+            self.press_box_and_check_status(x, y)
+        elif not self.matrix_controller.check_and_press(x, y):
+            self.game.lose_game()
+        elif not self.matrix_controller.get_boxes(is_hidden=True):
+            self.game.lose_game()
--- main/models.py
+++ main/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -21,6 +23,19 @@ class Box(models.Model):
 
     def sum(self):
         self.value += 1
+        self.save()
+
+    def press(self):
+        self.is_hidden = False
+        self.save()
+
+    def set_flag(self):
+        self.has_flag = True
+        self.save()
+
+    def put_mine(self):
+        self.value = self.MINE
+        self.save()
 
 
 class Game(models.Model):
@@ -37,8 +52,35 @@ class Game(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=INITIAL)
     started_time = models.DateTimeField()
+    ended_time = models.DateTimeField()
     matrix = models.ForeignKey(Matrix, on_delete=models.CASCADE)
 
     def get_status(self):
         return self.get_status_display()
 
+    def get_played_time(self):
+        if self.status != self.STARTED:
+            raise RuntimeError('Game not started yet')
+        return self.started_time - self.ended_time
+
+    def start_game(self):
+        self.status = self.STARTED
+        self.started_time = datetime.datetime.now()
+        self.save()
+
+    def lose_game(self):
+        self.status = self.LOST
+        self.ended_time = datetime.datetime.now()
+        self.save()
+
+    def win_game(self):
+        self.status = Game.WON
+        self.ended_time = datetime.datetime.now()
+        self.save()
+
+    def to_string(self):
+        return {
+            'user': self.user.username,
+            'status': self.get_status_display(),
+            'started_time': self.started_time.strftime('%m-%d-%Y %H:%M') if self.started_time else '-'
+        }
